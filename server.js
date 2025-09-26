ad954c2a518faeb12215d3754ae64d9eb41da066
miguelxxx <miguelparreiramedeiros@gmail.com>
2025-09-26 12:30:17 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
-const bodyParser = require("body-parser") // biblioteca que acabamos de isntalar pelo terminal
-const express = require("express") // biblioteca que instalamos 
-const app = express() // criar o servidor do app da biblioteca express
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
 
-app.use(express.static('.')) // criar o servidor
-app.use(bodyParser.urlencoded({
-    extended: true
-})) 
-app.use(bodyParser.json()) //  middleware para ler dados do corpo da requisição(formulario)
-const multer = require("multer") // middleware para lidar com uploads de arquivos 
- 
-const storage = multer.dickStorage({
-    destination: function(req, file, callback){
+
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// 
+const multer = require('multer')
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
         callback(null, './upload')
     },
-    filename: function(req, file, callback){
+    filename: function (req, file, callback) {
         callback(null, '${Date.now()}_${file.originalname}')
     }
-}) // Rotapost que recebe um arquivo
+})
 
-const upload = multer({storage }).single('arquivo')
-// para salvar o arquivo no destino correto
 
-app.post('/upload', (req, res) =>{
+const upload = multer({ storage }).single('arquivo')
+
+
+app.post('/upload', (req, res) => {
     upload(req, res, err => {
-        if(err){
+        if (err) {
             return res.end('Ocorreu um erro.')
         }
 
-        res.end('Concluído')
-}
-)
+        res.end('Concluído com sucesso.')
+    })
+})
+
+
+app.listen(8080, () => console.log('Executando...'))
